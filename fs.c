65895ad04f86012869fecc37742240f1c9375221
mchqueiroz <khaellzyn@gmail.com>
2026-07-08 18:42:55 -0300

    fs_write Adicionado

--- fs.c
+++ fs.c
@@ -29,6 +29,7 @@
 #define CLUSTERSIZE 4096
 #define FATCLUSTERS 65536
 #define DIRENTRIES 128
+#define MAXOPENFILES 10
 
 unsigned short fat[FATCLUSTERS];
 
@@ -40,6 +41,16 @@ typedef struct {
 } dir_entry;
 
 dir_entry dir[DIRENTRIES];
+
+typedef struct {
+  int used;            
+  int dir_index;       
+  int mode;             
+  int current_cluster;  
+  int cluster_offset;
+} open_file_entry;
+
+open_file_entry openfiles[MAXOPENFILES];
 
 
 int fs_init() {
@@ -110,8 +121,97 @@ int fs_close(int file)  {
 }
 
 int fs_write(char *buffer, int size, int file) {
-  printf("Função não implementada: fs_write\n");
-  return -1;
+  int i;
+  int escrito = 0;
+  char buf_temp[4096];
+
+  // Verifica se o arquivo é válido e está aberto para escrita
+  if (file < 0 || file > 9 || openfiles[file].used == 0) {
+    printf("Erro: arquivo fechado ou invalido\n");
+    return -1;
+  }
+  if (openfiles[file].mode != 1) { // 1 = FS_W
+    printf("Erro: arquivo nao ta aberto para escrita\n");
+    return -1;
+  }
+  if (size <= 0) {
+    return 0;
+  }
+
+  int id_dir = openfiles[file].dir_index;
+
+  // Loop para escrever os dados aos poucos
+  while (escrito < size) {
+    
+    // Se o arquivo é novo ou o bloco de 4096 bytes já encheu
+    if (openfiles[file].current_cluster == -1 || openfiles[file].cluster_offset == 4096) {
+      
+      // Procura um bloco livre na FAT
+      int novo_bloco = -1;
+      for (i = 33; i < 65536; i++) {
+        if (fat[i] == 1) {
+          novo_bloco = i;
+          break;
+        }
+      }
+
+      if (novo_bloco == -1) {
+        printf("Erro: disco cheio!\n");
+        break; // Sai do loop se não tiver espaço
+      }
+
+      // Atualiza a FAT
+      if (openfiles[file].current_cluster == -1) {
+        dir[id_dir].first_block = novo_bloco; // Primeiro bloco do arquivo
+      } else {
+        fat[openfiles[file].current_cluster] = novo_bloco; // Liga o bloco velho ao novo
+      }
+      
+      fat[novo_bloco] = 2;
+      
+      // Atualiza o arquivo aberto
+      openfiles[file].current_cluster = novo_bloco;
+      openfiles[file].cluster_offset = 0;
+    }
+
+    // Le o que já está no disco para o buffer temporario para não apagar coisas sem querer
+    bl_read(openfiles[file].current_cluster, buf_temp);
+
+    // Calcula quanto espaço tem e quanto ainda falta escrever
+    int espaco = 4096 - openfiles[file].cluster_offset;
+    int falta = size - escrito;
+    int copiar;
+    
+    if (falta < espaco) {
+      copiar = falta;
+    } else {
+      copiar = espaco;
+    }
+
+    // Copia os dados do buffer do utilizador para o nosso buffer
+    memcpy(buf_temp + openfiles[file].cluster_offset, buffer + escrito, copiar);
+
+    // Grava o bloco modificado de volta no disco
+    bl_write(openfiles[file].current_cluster, buf_temp);
+
+    // Avança os contadores
+    openfiles[file].cluster_offset += copiar;
+    escrito += copiar;
+  }
+
+  //Atualiza o tamanho do arquivo no diretorio somando o que foi escrito
+  dir[id_dir].size += escrito;
+  
+  // Salva os 32 blocos da FAT
+  char *p_fat = (char *)fat;
+  for (i = 0; i < 32; i++) {
+    bl_write(i, p_fat + (i * 4096));
+  }
+
+  // Salva o bloco 32 que é o Diretorio
+  bl_write(32, (char *)dir);
+
+  return escrito; // Retorna o total de bytes que conseguiu escrever
 }
 
 int fs_read(char *buffer, int size, int file) {
